086e2dd874808cb13f987af0a5cfb04933789f8e
mwcode.fun@gmail.com <mwcode.fun@gmail.com>
2021-10-14 17:04:28 +0800

    a

--- _install/install_server.sh
+++ _install/install_server.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-if [[ "$SHELL" == "/bin/bash" ]]; then
-    echo "append profile"
-    cat ./profile/server.profile > $HOME/.profile
-    source $HOME/.profile
-    echo "append profile success"
-fi
+clone-or-download(){
+    if ! command -v git &> /dev/null
+    then
+        wget -O mw-dev-tool.zip https://codeload.github.com/mwcodefun/mw-dev-tool/zip/refs/heads/master
+        unzip mw-dev-tool.zip
+        cd mw-dev-tool-master
+    else
+        git clone git@github.com:mwcodefun/mw-dev-tool.git
+        cd mw-dev-tool
+        rm -r -f .git
+    fi
+}
+install_profile(){
+    if [[ "$SHELL" == "/bin/bash" ]]; then
+        echo "append profile"
+        cat ./profile/server.profile > $HOME/.profile
+        source $HOME/.profile
+        echo "append profile success"
+    fi
+}
+
+clone-or-download
+install_profile
